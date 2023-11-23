03befdf9438597017af1d2ffa62137071e5a7bc1
washington-shoji <shoji.washington@gmail.com>
2023-11-23 21:51:34 +1100

    -added product routing and updated module

--- src/app/feature/product/product.module.ts
+++ src/app/feature/product/product.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ProductRoutingModule } from './product-routing.module';
+import { ProductScreenComponent } from './product-screen/product-screen.component';
+import { HttpClientModule } from "@angular/common/http";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductScreenComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductRoutingModule,
+    HttpClientModule
   ]
 })
 export class ProductModule { }
