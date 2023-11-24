09e5fa88d2db60b729dd7f7eabbb3e4fb77e0da5
washington-shoji <shoji.washington@gmail.com>
2023-11-24 13:17:27 +1100

    - added simple http get method to fetch product data - added simple html elements and scss styling to render the data - added a few todos as homework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductModule } from "./feature/product";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/feature/product/index.ts
@@ -0,0 +1,2 @@
+export * from './product.module';
+export * from './product-screen/product-screen.component';
