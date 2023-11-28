9cb93d7df07d8decb026eea199a967a28a9eb942
washington-shoji <shoji.washington@gmail.com>
2023-11-28 16:49:30 +1100

    - updated product card component with @Input and @Output data flow

--- src/app/feature/product/product-card/product-card.component.ts
+++ src/app/feature/product/product-card/product-card.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Product} from "../prooduct-type";
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
-export class ProductCardComponent implements OnInit {
+export class ProductCardComponent implements OnInit, OnChanges {
+  @Input() product: Product;
+  @Input() category: string;
 
-  constructor() { }
+  constructor() {
+    this.category = "";
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(caches) {
+      console.warn("changes", changes)
+    }
   }
 
 }
