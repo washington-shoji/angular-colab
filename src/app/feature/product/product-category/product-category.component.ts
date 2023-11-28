acccf9a4ba2d3589b2dff89138a7eb8fc107e87c
washington-shoji <shoji.washington@gmail.com>
2023-11-28 16:49:54 +1100

    - updated product category component with @Input and @Output data flow

--- src/app/feature/product/product-category/product-category.component.ts
+++ src/app/feature/product/product-category/product-category.component.ts
@@ -1,17 +1,4 @@
-import { Component, OnInit , OnDestroy } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Subscription } from 'rxjs';
-
-//interface
-
-interface Category {
-  'title': string
-}
-
-interface Categories {
-  categories: Category[];
-}
-
+import {Component, OnInit, OnDestroy, Input, Output, EventEmitter} from '@angular/core';
 
 
 @Component({
@@ -20,45 +7,23 @@ interface Categories {
   styleUrls: ['./product-category.component.scss']
 })
 export class ProductCategoryComponent implements OnInit, OnDestroy {
+  @Input() category: string = "";
+  @Output() categoryFromChildEvent: EventEmitter<string> = new EventEmitter<string>();
 
-  private _subscription: Subscription;
-  categories: Category[];
-  fakeAPIUrl = "https://dummyjson.com/products/categories"
+  constructor() {
 
-  isLoading: boolean = false;
-  errorMessage: string | null = null;
-
-  constructor(private http: HttpClient) {
-    this._subscription = new Subscription();
-    this.categories = [];
   }
 
   ngOnInit(): void {
-    // this.fetchDataInit();
+
   }
 
   ngOnDestroy(): void {
-    this._subscription.unsubscribe();
+
   }
 
-  fetchDataInit(): void {
-    this.isLoading = true;
-    this._subscription.add(
-      this.http.get<Categories>(this.fakeAPIUrl).subscribe({
-        next: (data) => {
-          // Means success
-          this.isLoading = false;
-          this.categories = data.categories;
-          console.warn('data', data);
-        },
-        error: (error) => {
-          // Means error
-          this.isLoading = false;
-          this.errorMessage = error.message;
-          console.warn('error', error)
-        },
-        complete: () => {}
-      })
-    );
+  sendCategory(categoryItem: string): void {
+    this.categoryFromChildEvent.emit(categoryItem);
   }
+
 }
