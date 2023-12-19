236181efbfce88ac082604c491be7b42d1a036a3
washington-shoji <shoji.washington@gmail.com>
2023-12-19 12:47:03 +1100

    -added doashboard screen component

---
+++ src/app/feature/dashboard/dashboard-screen/dashboard-screen.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DashboardScreenComponent } from './dashboard-screen.component';
+
+describe('DashboardScreenComponent', () => {
+  let component: DashboardScreenComponent;
+  let fixture: ComponentFixture<DashboardScreenComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DashboardScreenComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DashboardScreenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
